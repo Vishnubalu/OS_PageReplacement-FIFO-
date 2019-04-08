c516994bc513bc71b1d35e46398891b880f86dff
Vishnubalu <38403733+Vishnubalu@users.noreply.github.com>
2019-04-09 01:09:48 +0530

    Update pagereplacement.c

--- pagereplacement.c
+++ pagereplacement.c
@@ -1,13 +1,17 @@
+#include<unistd.h>
+#include<time.h>
 #include<stdio.h>
+#include<stdlib.h>
 int pagereplacement(int k, int pages[],int n)
 {
+
       int frame[k];
       for(int i=0; i<k; i++)
       {
         frame[i] = -1;
       }
     int faults=0, hit=0;
-    int len_frame =  k; //sizeof(frame)/sizeof(int);
+    int len_frame =  k; 
     int len = len_frame;
 
     for(int i=0; i<n; i++)
@@ -35,15 +39,25 @@ int pagereplacement(int k, int pages[],int n)
               faults++;
           }
     }
-    printf("no. of frames : %d      | faults : %d    hits   : %d\n", k, faults, hit);
-//}
+    printf("\n no. of frames : %d      | faults : %d    hits   : %d\n", k, faults, hit);
 }
 int main()
+
 {
-  int pages[] = {1,2,3,4,2,1,5,6,2,1,2,3,7,6,3,2,1,2,3,6};
+  int pages[15];
+ srand(time(NULL)); 
+for(int i=0;i<15;i++)
+{
+    pages[i] = rand()%9;
+}
+printf("Page Frames are : ");
+for(int i=0;i<15;i++)
+{
+  printf("%d", pages[i]);
+}
   for(int k=1; k<8; k++)
   {
     pagereplacement(k, pages, sizeof(pages)/sizeof(int));
   }
-
+printf("%ld", sizeof(pages)/sizeof(int));
 }
